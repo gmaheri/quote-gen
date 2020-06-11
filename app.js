2b9293d3ffeeef447e989d9b938a54e0fc9de5a0
Gregory <gregory.maheri@gmail.com>
2020-06-11 23:41:39 +0300

    added fetch API

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var quotes = [
-  "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver. - Any Rand",
-  "Not he who has much is rich, but he who gives much. - Erich Fromm",
-  "Time is more valuable than money. You can get more money, but you cannot get more time. - Jim Rohn",
-  "If you would be wealthy, think of saving as well as getting. - Ben Franklin"
-];
-const getQuote = () => {
-  var randomNumber = Math.floor(Math.random() * quotes.length);
-  document.getElementById('newQuoteSection').innerHTML = quotes[randomNumber];
+
+const getCatFact = () => {
+  fetch("https://api.quotable.io/random")
+  .then((res) => {
+    return res.json();
+  })
+  .then(data => {
+    document.getElementById('newQuoteSection').innerHTML = data.content;
+    document.getElementById('author').innerHTML = data.author
+  })
+  .catch(err => console.error(err))
 }
